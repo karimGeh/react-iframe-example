095569066013db48c26355dc9c7a8a7c517629b1
karimGeh <karim2jihad@gmail.com>
2024-05-13 23:41:34 +0100

    update iframe link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-const GAME_URL = "http://127.0.0.1:5500/";
+const GAME_URL = "https://iframe-games-examples.vercel.app/";
 
 function App() {
   const ref = useRef<HTMLIFrameElement>(null);
